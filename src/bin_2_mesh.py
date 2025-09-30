fd517bbc90f849e7f6ffb0e35205eac2bb107655
Matt <m.b.smith-5@umcutrecht.nl>
2025-09-30 20:30:54 +0200

    Both models "work" this is for syncing and tracking changes.

---
+++ src/bin_2_mesh.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python3
+
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+
+import ngff_zarr
+import numpy
+import struct
+import sys, json, math, os
+
+import binarymeshformat as bmf
+
+"""
+This will use a NN to create a mesh output from a cropped image.
+The structure of the meshes is well defined defined each mesh has a
+fixed number of points.
+
+The input will be an image crop and the output will be the positions of
+the mesh.
+
+"""
+
+base_config = {
+    "images" : "masks.zarr",
+    "meshes" : "qm_mesh-masks.dat",
+    "model" : "dev-model.pth",
+    "latent_size" : 64,
+    "filters" : 16,
+    "max_pools": 3,
+    "batch_size" : 128
+    }
+
+
+
+
+class SkipLayer(nn.Module):
+    """
+        Three conv3d layers, two consecutive and one that skips.
+    """
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.a = nn.Sequential(
+            nn.Conv3d( in_c, out_c//2, 3, padding="same" ),
+            nn.Conv3d( out_c//2, out_c//2, 3, padding="same"),
+            nn.ReLU() )
+        self.b = nn.Sequential( nn.Conv3d( in_c, out_c//2, 3, padding="same" ), nn.ReLU() )
+
+    def forward(self, x):
+        n0 = self.a(x)
+        n1 = self.b(x)
+        return torch.cat([n0, n1], axis=1)
+
+
+
+class BinaryToMeshTransformer(nn.Module):
+    """
+        The model used for creating meshes.
+    """
+    def __init__(self, latent_size, filters, max_pools):
+        super().__init__()
+        layers = []
+        n = filters
+        current = 1
+        for i in range(3):
+            layers.append( SkipLayer( current, n) )
+            current = n
+            n = 2*n
+            if i < max_pools:
+                layers.append( nn.MaxPool3d(2) )
+        layers.append( SkipLayer(current, 2) )
+        layers.append( nn.Flatten() )
+        length = 48//2**max_pools
+        total = length**3*2
+        layers.append( nn.Linear( total, latent_size ) )
+        layers.append( nn.ReLU() )
+        layers.append( nn.Linear( latent_size, 1926) )
+        
+        self.encoder = nn.Sequential(  *layers )
+    def forward(self, x):
+        return self.encoder(x)
+
+def loadModel( config ):
+    return BinaryToMeshTransformer( config["latent_size"], config["filters"], config["max_pools"] )
+
+def loadMeshFile( mesh_file, limit=-1 ):
+    with open(mesh_file, 'rb') as opend:
+        n = opend.read(4)
+        n_indexes = struct.unpack_from(">i", n, 0)[0]
+        n_bytes = n_indexes*4
+        tindexes = struct.unpack_from(">%si"%n_indexes, opend.read(n_bytes), 0)
+
+        n = opend.read(4)
+        n_indexes = struct.unpack_from(">i", n, 0)[0]
+        n_bytes = n_indexes*4
+        cindexes = struct.unpack_from(">%si"%n_indexes, opend.read(n_bytes), 0)
+
+        n = opend.read(4)
+        values = []
+
+        frame = 0
+        while len(n) == 4:
+            n_floats = struct.unpack_from(">i", n, 0)[0]
+            n_bytes = n_floats*8
+            f_bytes = opend.read(n_bytes)
+            if len(f_bytes) != n_bytes:
+                print("final mesh truncated!")
+                break
+            positions = struct.unpack_from(">%sd"%n_floats, f_bytes, 0)
+            if any( math.isnan(x) for x in positions ):
+                print(frame, "broken mesh")
+            else:
+                values.append(numpy.array(positions, dtype="float32"))
+            n = opend.read(4)
+            
+            frame += 1
+            if frame==limit:
+                break
+        return {"triangles" : tindexes,"connections":cindexes}, numpy.array(values, dtype="float32")
+
+class ImageMeshDataset(Dataset):
+    """
+        The images are crops saved as zarr files. The meshes are a
+    binary file.
+
+
+    """
+    def __init__(self, img_path, mesh_path):
+        indexes, meshes = loadMeshFile(mesh_path)
+        print(meshes.shape)
+        multiscales = ngff_zarr.from_ngff_zarr(img_path)
+        top = multiscales.images[0].data
+        self.meshes = meshes;
+        self.images = top[:meshes.shape[0]]
+        self.topo = indexes
+
+
+    def __len__(self):
+        return self.meshes.shape[0]
+
+    def __getitem__(self, idx):
+        return numpy.array(self.images[idx], dtype="float32"), numpy.array(self.meshes[idx], dtype="float32")
+
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    model.train()
+    acc = 0.0
+    n = 0
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction error
+        X = X.to(device)
+        y = y.to(device)
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        acc += loss
+        n += 1
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+    return acc/n
+
+
+
+def trainModel( config ):
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    print(f"Using {device} device")
+    images = config["images"]
+    meshes = config["meshes"]
+    dataset = ImageMeshDataset(images, meshes)
+    loader = DataLoader(dataset, batch_size=config["batch_size"], shuffle = True)
+    transformer = loadModel(config)
+    
+    if os.path.exists( config["model"] ):
+        transformer.load_state_dict(torch.load(config["model"], weights_only=True))
+
+    model  = transformer.to(device)
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.Adam(transformer.parameters(), lr = 0.001)
+    logname = config["model"].replace(".pth", "")
+    for i in range(1000):
+        l = train(loader, model, loss_fn, optimizer, device)
+        with open("log-%s.txt"%logname, 'a') as logit:
+            logit.write("%s\t%s\n"%(i, l.item() ) )
+        torch.save(model.state_dict(), config["model"])
+
+def predictMeshes( config ):
+    transformer = loadModel(config)
+    transformer.load_state_dict(torch.load(config["model"], weights_only=True))
+    images = config["images"]
+    meshes = config["meshes"]
+    dataset = ImageMeshDataset(images, meshes)
+    topo = dataset.topo
+    loader = DataLoader(dataset, batch_size=64, shuffle = False)
+    track = bmf.Track("predicted")
+
+    dex = 0
+    for i, (X, y) in enumerate(loader):
+        z = transformer(X)
+        for row in z:
+            mesh = bmf.Mesh(row.detach(), topo["connections"], topo["triangles"])
+            track.addMesh( dex, mesh)
+            dex += 1
+    bmf_name = "dev-%s.bmf"%config["model"].replace(".pth", "")
+    bmf.saveMeshTracks( [track], bmf_name)
+
+def saveMeshes(config):
+    n = 64;
+    topo, positions = loadMeshFile(config["meshes"], limit=n)
+    track = bmf.Track("truth")
+    for i in range(n):
+        mesh = bmf.Mesh(positions[i], topo["connections"], topo["triangles"])
+        track.addMesh( i, mesh)
+    bmf.saveMeshTracks([track], "dev-truth.bmf")
+
+def getConfig(config_name):
+    config = {}
+    if os.path.exists(config_name):
+        with open(sys.argv[2], 'r') as fp:
+            config = json.load(fp)
+    else:
+        config.update(base_config)
+        json.dump( config, open(sys.argv[2], 'w'), indent = 1 )
+    return config
+if __name__=="__main__":
+    config = getConfig(sys.argv[2])
+    if sys.argv[1] == 't':
+        trainModel(config)
+    elif sys.argv[1] == 'p':
+        predictMeshes(config)
+    elif sys.argv[1]=='e':
+        saveMeshes(config)
---
+++ src/img_2_dt.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+
+
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+
+import ngff_zarr
+
+import scipy, numpy
+import sys, os, json
+
+base_config = {
+    "images" : "images.zarr",
+    "masks" : "masks.zarr",
+    "model" : "dt-model.pth",
+    "filters" : 16,
+    "depth": 3,
+    "batch_size" : 128
+    }
+
+def getConfig(config_name):
+    config = {}
+    if os.path.exists(config_name):
+        with open(sys.argv[2], 'r') as fp:
+            config = json.load(fp)
+    else:
+        config.update(base_config)
+        json.dump( config, open(sys.argv[2], 'w'), indent = 1 )
+    return config
+
+class SkipLayer(nn.Module):
+    """
+        Three conv3d layers, two consecutive and one that skips.
+    """
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.a = nn.Sequential(
+            nn.Conv3d( in_c, out_c//2, 3, padding="same" ),
+            nn.Conv3d( out_c//2, out_c//2, 3, padding="same"),
+            nn.ReLU() )
+        self.b = nn.Sequential( nn.Conv3d( in_c, out_c//2, 3, padding="same" ), nn.ReLU() )
+
+    def forward(self, x):
+        n0 = self.a(x)
+        n1 = self.b(x)
+        return torch.cat([n0, n1], axis=1)
+
+class Upscale(nn.Module):
+    def __init__(self, filters):
+        super().__init__()
+        self.f = torch.nn.ConvTranspose3d( filters, filters, kernel_size = 2, stride = 2 )
+    def forward(self, x):
+        return self.f(x)
+
+class ImageToDistanceTransform(nn.Module):
+    """
+        The model used for creating meshes.
+    """
+    def __init__(self, filters, depth):
+        super().__init__()
+        self.contracting = nn.ModuleList()
+        self.expanding = nn.ModuleList()
+        n = filters
+        current = 1
+        for i in range(depth):
+            if( i > 0):
+                self.contracting.append( nn.Sequential( nn.MaxPool3d(2), SkipLayer( current, n ) ) )
+            else:
+                self.contracting.append( SkipLayer( current, n ) )
+            current = n
+            n = 2*n
+
+        for i in range(depth - 1):
+            if( i > 0 ):
+                self.expanding.append( nn.Sequential( SkipLayer( 2*current, current//2), Upscale(current//2) ) )
+            else:
+                self.expanding.append( nn.Sequential( SkipLayer( current, current//2 ), Upscale(current//2) ) )
+            current = current // 2
+        self.expanding.append( SkipLayer( 2*current, current//2 ) )
+        current = current//2
+        self.output = nn.Conv3d( current, 1, 3, padding="same" )
+
+    def forward(self, x):
+        skips = []
+        x0 = x
+        for layer in self.contracting:
+            x0 = layer( x0 )
+            skips.append( x0 )
+        ns = len(skips)
+        for i, layer in enumerate(self.expanding):
+            if i > 0:
+                #last layer of skips is not getting passed across.
+                y0 = skips[ns - i - 1]
+                x0 = torch.cat([y0, x0], axis=1)
+            x0 = layer(x0)
+        return self.output(x0)
+
+
+class ImageDtDataset(Dataset):
+    """
+        The images are crops saved as zarr files. The meshes are a
+    binary file.
+
+
+    """
+    def __init__(self, img_path, mask_path):
+        multiscales = ngff_zarr.from_ngff_zarr(img_path)
+        img = multiscales.images[0].data
+        multiscales = ngff_zarr.from_ngff_zarr(img_path)
+        msk = multiscales.images[0].data
+        self.masks = msk
+        self.images = img
+
+
+    def __len__(self):
+        return self.images.shape[0]
+
+    def __getitem__(self, idx):
+        x = numpy.array(self.images[idx], dtype="float32")
+        y = numpy.array(scipy.ndimage.distance_transform_edt( self.masks[idx]), dtype="float32")
+        return x,y
+
+
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    model.to(device)
+    model.train()
+    acc = 0.0
+    n = 0
+    for batch, (X, y) in enumerate(dataloader):
+        X = X.to(device)
+        y = y.to(device)
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        acc += loss
+        n += 1
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+    return acc/n
+
+def trainModel( config ):
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    print(f"Using {device} device")
+
+    model = ImageToDistanceTransform( config["filters"], config["depth"])
+    if os.path.exists( config["model"] ):
+        model.load_state_dict(torch.load(config["model"], weights_only=True))
+
+    dataset = ImageDtDataset( config["images"], config["masks"] )
+    loader = DataLoader(dataset, batch_size=config["batch_size"], shuffle = True)
+    logname = config["model"].replace(".pth", "")
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.00001)
+    loss_fn = nn.MSELoss()
+
+    for i in range(1000):
+        l = train(loader, model, loss_fn, optimizer, device)
+        with open("log-%s.txt"%logname, 'a') as logit:
+            logit.write("%s\t%s\n"%(i, l.item() ) )
+        torch.save(model.state_dict(), config["model"])
+
+
+def predictDt( config ):
+    pass
+
+
+if __name__=="__main__":
+    config = getConfig(sys.argv[2])
+    if sys.argv[1] == 't':
+        trainModel(config)
+    elif sys.argv[1] == 'p':
+        predictDt(config)
+
